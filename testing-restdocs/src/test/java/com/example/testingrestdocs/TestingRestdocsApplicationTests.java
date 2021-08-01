bdbe361c5d9741614dd47d3053b00d01978930bd
yoo-jaein <y_jaein@naver.com>
2021-08-01 16:26:15 +0900

    폴더 위치 변경

---
+++ testing-restdocs/src/test/java/com/example/testingrestdocs/TestingRestdocsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.testingrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingRestdocsApplicationTests {
+
+	@Test
+	public void contextLoads() throws Exception {
+	}
+}
